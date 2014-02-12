8e30bbb8cea0f709c3095cb0394cc19fcc1bc128
prashantc29 <prash.c.29@gmail.com>
2014-02-12 02:57:36 -0600

    Adding RMI constants in common package #11

---
+++ common/src/main/java/edu/umn/pubsub/common/constants/RMIConstants.java
@@ -0,0 +1,9 @@
+package edu.umn.pubsub.common.constants;
+
+public final class RMIConstants {
+	private RMIConstants(){
+		// Constants class so cannot instantiate
+	}
+	
+	public static final String PUB_SUB_SERVICE = "PubSubService";
+}
