aadc5ddbd78767735dc944d4d087cdf0b1aa6982
prashantc29 <prash.c.29@gmail.com>
2014-02-18 16:02:52 -0600

    #14

---
+++ common/src/main/java/edu/umn/pubsub/common/constants/RegisteryServerConstants.java
@@ -0,0 +1,10 @@
+package edu.umn.pubsub.common.constants;
+
+public final class RegisteryServerConstants {
+
+	private RegisteryServerConstants() {
+		
+	}
+	
+	public static final String HEARTBEAT = "heartbeat";
+}
